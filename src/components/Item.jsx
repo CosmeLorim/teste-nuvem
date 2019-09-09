933017d2c4e54b13e9b501fb2e320310dc44976b
Cosme Lorim <cosme.lorim@gmail.com>
2019-09-09 04:20:50 -0400

    Cria pagina de listagem de pratos (#2)
    * Remove arquivos sem uso
    
    * Adiciona pacotes
    - Axios e prop-types.
    
    * Cria listagem de produtos
    - Busca dados na API;
    - Gerencia estado local usando useState;
    - Cria componente para mostrar produto;
    - Renderiza lista de produtos.

---
+++ src/components/Item.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+import { Row, Col } from 'antd'
+
+const styles = {
+  row: {
+    padding: '10px'
+  },
+  price: {
+    color: 'green'
+  },
+  img: {
+    width: '100px',
+    borderRadius: '4px'
+  }
+}
+
+const Item = ({ name, price, urlImage, altImage }) => {
+  price = price.toLocaleString('pt-br', { minimumFractionDigits: 2 })
+  return (
+    <Row gutter={16} style={styles.row}>
+      <Col xs={16}>
+        <h3>{name}</h3>
+        <p style={styles.price}>R$ {price}</p>
+      </Col>
+      <Col xs={8}>
+        <img src={urlImage} style={styles.img} alt={altImage} />
+      </Col>
+    </Row>
+  )
+}
+
+Item.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  urlImage: PropTypes.string.isRequired,
+  altImage: PropTypes.string
+}
+
+export default Item
