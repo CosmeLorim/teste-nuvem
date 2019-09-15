6fb806f7d7f74f7cb9ffb2cd5e8bf47a7bcd097a
Cosme Lorim <cosme.lorim@gmail.com>
2019-09-15 18:16:46 -0400

    Finaliza componente "type-sigle"

--- src/components/modal-additional/type-single.jsx
+++ src/components/modal-additional/type-single.jsx
@@ -1,31 +1,56 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Card, Radio } from 'antd'
+import { Radio, Form, Typography } from 'antd'
 
-const style = {
-  radio: {
-    display: 'block',
-    height: '30px',
-    lineHeight: '30px'
+import styles from './type-sigle.styles.js'
+
+// Função para caso o usuário clique sobre o opção já selecionada, neste caso deve remover a seleção
+const removeSellection = ({ form, id, value }) => {
+  const fieldValue = form.getFieldValue(id)
+  // Caso o campo clicado seja o já selecionado remove a seleção do Radio Button
+  if (value === fieldValue) {
+    form.setFieldsValue({ [id]: undefined })
   }
 }
 
-const TypeSingle = ({ title, values }) => {
-  const radiosButton = values.map(v => <Radio key={`option${v.id}`} style={style.radio} value={v.id}>{v.name}</Radio>)
+const renderRadioButton = ({ form, id }) => v => {
+  const price = v.price.toLocaleString('pt-br', { minimumFractionDigits: 2 })
+  return (
+    <div key={`option${v.id}`}>
+      <Radio style={styles.radio} value={v.id} onClick={e => removeSellection({ form, id, value: v.id })}>
+        {v.name}
+      </Radio>
+      <Typography.Paragraph style={styles.paragraph}> + R$ {price} </Typography.Paragraph>
+    </div>
+  )
+}
+
+const TypeSingle = ({ id, title, values, form, required }) => {
+  const { getFieldDecorator } = form
+  const radiosButton = values.map(renderRadioButton({ form, id }))
 
   return (
-    <Card title={title}>
-      <Radio.Group>
-        {radiosButton}
-      </Radio.Group>
-    </Card>
+    <Form.Item label={title}>
+      {getFieldDecorator(id, {
+        rules: [{ required, message: 'Necessário escolher uma opção' }]
+      })(
+        <Radio.Group>
+          {radiosButton}
+        </Radio.Group>
+      )}
+    </Form.Item>
   )
 }
 
 TypeSingle.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  required: PropTypes.bool.isRequired,
+  form: PropTypes.object,
   values: PropTypes.arrayOf(PropTypes.shape({
-
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
   }))
 }
 
